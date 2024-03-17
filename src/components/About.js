9caf4d56f91018b7d03b1ca11c515f5adbabe345
Danial Sadad <110252752+danialsadad@users.noreply.github.com>
2024-03-17 16:51:26 +0600

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div>
+            <h4>Greetings,It helps to keep track of your works, todos and also reminds you. Thank You for using!</h4>
+            <Link to='/'>Go Back</Link>
+        </div>
+    )
+}
+
+export default About
